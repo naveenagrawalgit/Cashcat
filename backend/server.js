42115db288b94ba04ef0fb4e0ba77299e2a08ad1
naveen <naveenagrawal1008@gmail.com>
2025-07-18 23:09:48 +0530

    transaction route and transaction table

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,33 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import { sql } from './confiq/db.js';
 
 dotenv.config();
 
 
 const app = express()
 
+app.use(express.json())
 
 async function initDB(){
-
-
     try {
-        await gg
+        await sql`CREATE TABLE IF NOT EXISTS transactions(
+        id SERIAL PRIMARY KEY,
+        user_id VARCHAR(255) NOT NULL,
+        title VARCHAR(255) NOT NULL,
+        amount DECIMAL(10,2) NOT NULL,
+        category VARCHAR(255) NOT NULL,
+        created_at DATE NOT NULL DEFAULT CURRENT_DATE
+        )`;
+
+        console.log('database initialized')
+
     } catch (error) {
+
+        console.log("Error initializing DB", error)
         
     }
-
-
 }
-
-
 
 
 
@@ -28,7 +36,41 @@ app.get("/", (req,res)=>{
     res.send("server is running at :--")
 })
  
-app.listen(process.env.PORT,()=>{
+app.get("/api/transaction",async(req,res) => {
+    try{
+        const {title, amount, category,user_id} = req.body;
+
+        if(!title || !user_id || !category || amount=== undefined){
+            return res.status(400).json({message: "All fields are required"});
+        }
+
+        const transaction = await sql`INSERT INTO transactions(user_id,title,amount,category)
+        VALUES (${user_id},${title},${amount},${category})
+        RETURNING *
+        `;
+        console.log(transaction)
+        res.status(201).json(transaction[0])
+
+
+    }
+    catch(err){
+
+        console.log("error in form data submission", err)
+    }
+})
+
+
+initDB().then(()=>{
+
+
+    app.listen(process.env.PORT,()=>{
     console.log("server is running at :-- ", process.env.PORT)
 })
+
+})
+
+
+
+
+
 
