4b63bb5a8609d5c048a1e2c4da9359675e384798
naveen <naveenagrawal1008@gmail.com>
2025-08-09 15:53:17 +0530

    cron job commit

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,20 @@ import dotenv from 'dotenv'
 import { initDB } from './confiq/db.js';
 import rateLimiter from './middleware/rateLimiter.js';
 import transactionsRoute from './routes/transactions.Route.js';
+import job from './confiq/cron.js';
 dotenv.config();
 
-
 const app = express()
+
+if(process.env.NODE_ENV === 'production') job.start()
 
 app.use(express.json())
 
 app.use(rateLimiter)
 
-
+app.get('/api/health',(req,res)=>{
+    res.status(200).json({status: "ok"});
+})
 
 app.use("/api", transactionsRoute)
 
